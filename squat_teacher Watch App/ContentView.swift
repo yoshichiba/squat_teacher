cf7e2f2673f582cc229a0228d0e9c879cc1b627a
yoshichibaaa <yoshi19941117@gmail.com>
2025-09-14 13:02:02 +0900

    Initial Commit

---
+++ squat_teacher Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  squat_teacher Watch App
+//
+//  Created by 千葉良晴 on 2025/09/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ squat_teacher Watch App/squat_teacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  squat_teacherApp.swift
+//  squat_teacher Watch App
+//
+//  Created by 千葉良晴 on 2025/09/14.
+//
+
+import SwiftUI
+
+@main
+struct squat_teacher_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
